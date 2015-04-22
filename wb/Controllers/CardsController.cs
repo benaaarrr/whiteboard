9a5b2f52ec2782ccd94fca2c54aa4ba3f00f87c2
benaaarrr <bengodwin@live.co.uk>
2015-04-22 17:43:41 +0100

    - loads of changes

---
+++ wb/Controllers/CardsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wb.Models;
+
+namespace wb.Controllers
+{
+    public class CardsController : Controller
+    {
+        private CardContext db = new CardContext();
+
+        // GET: Cards
+        
+        public JsonResult Load()
+        {            
+            var card = db.Cards.Take(1).OrderByDescending(c => c.CreatedOn).Single();
+            return Json(card, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public JsonResult Save(string cardHtml)
+        {
+            var success = false;
+            try
+            {
+                if (cardHtml.Length > 0)
+                {
+                    db.Cards.Add(new Card() { CreatedOn = DateTime.Now, Html = cardHtml });
+                    db.SaveChanges();
+                }
+                success = true;
+            }
+            catch(Exception ex)
+            {
+                success = false;
+            }
+            
+            return Json(new {result = success});
+        }
+    }
+}
---
+++ wb/Migrations/201504221224415_InitalCreate.cs
@@ -0,0 +1,27 @@
+namespace wb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitalCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cards",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Html = c.String(maxLength: 4000),
+                        CreatedOn = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cards");
+        }
+    }
+}
